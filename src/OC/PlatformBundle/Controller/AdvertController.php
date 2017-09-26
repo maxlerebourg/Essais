b36c2c9f71071850aedfa1dce4762cb3f45fda4a
Max <maxlerebourg@gmail.com>
2017-09-26 10:43:06 +0200

    First !!

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+
+namespace OC\PlatformBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AdvertController
+
+{
+
+    public function indexAction()
+
+    {
+
+        return new Response("Notre propre Hello World !");
+
+    }
+
+}
